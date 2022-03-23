983132ec98c5bd3d98dc2a9daaf2cc699bb0b811
Karlis Berzins <berzinsh.karlis@gmail.com>
2022-03-23 07:06:34 +0200

    Initial commit

---
+++ emoji-printer/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  emoji-printer
+//
+//  Created by karlis.berzins on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
---
+++ emoji-printer/emoji_printerApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_printerApp.swift
+//  emoji-printer
+//
+//  Created by karlis.berzins on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct emoji_printerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
